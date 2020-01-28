ab2f2eb794ffc8f57ebd6f8dd1c26ce47d93c5f7
WizAom <Aom_ll2555@hotmail.com>
2020-01-28 16:34:33 +0700

    1st

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,19 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+int main(){
+    ifstream cheerbook;
+    ofstream cheerbook_copy;
+    string text;
+    cheerbook.open("D:\\lab9-2562-2-WizAom\\cheerbook.txt");
+    cheerbook_copy.open("D:\\lab9-2562-2-WizAom\\cheerbook_copy.txt");
+    cheerbook_copy << "-------------------- SOTUS ---------------------\n";
+    while(getline(cheerbook,text)){
+        cheerbook_copy << text << "\n" ;
+    }
+    cheerbook.close();
+    cheerbook_copy << "-------------------- SOTUS ---------------------";
+    cheerbook_copy.close();
+    return 0;
+}
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,22 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cmath>
+using namespace std;
+int main(){
+    ifstream score;
+    double m,x,n=0,sum=0,sum2=0;
+    string text;
+    score.open("D:\\lab9-2562-2-WizAom\\score.txt");
+    while(getline(score,text)){
+        sum+=atof(text.c_str());
+        n++;
+        sum2+=pow(atof(text.c_str()),2);
+    }
+    m=(1/n)*sum;
+    cout << "Number of data = " << n << "\n";
+    cout << "Mean = " << m << "\n";
+    cout << "Standard deviation = " << sqrt(((1/n)*sum2)-pow(m,2)) << "\n";
+    score.close();
+    return 0;
+}
